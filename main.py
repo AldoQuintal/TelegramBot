9275404a1be9b46f82a0ebf7493039bca982af85
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-04-01 09:53:19 -0600

    carga msj

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import telebot
 TOKEN = '6820969878:AAHM9yHnZ_LQF_PS0kyWWWFqSdflweEx75g'
 bot = telebot.TeleBot(TOKEN)
 
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hola bola')
+    
+    
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+    
+    
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
