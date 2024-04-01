69c9058c2356450d509b42d347a244ada4082de7
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-04-01 10:35:04 -0600

    tanques

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import telebot
+import requests
 
 TOKEN = '6820969878:AAHM9yHnZ_LQF_PS0kyWWWFqSdflweEx75g'
+url = 'https://10.10.208:8000/api/entregas'
+
+api_inv = 'http://10.10.208:8000/api/inventarios'
+api_entr = 'http://10.10.208:8000/api/entregas'
+
+
+
 bot = telebot.TeleBot(TOKEN)
+
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hola bola')
     
+    response = requests.get(url=api_inv,auth=None,verify=False)
+    tanques = response.json()
+    print(f'json_response: {tanques}')
+    bot.reply_to(message, 'Hola bola')
+        
     
 
 @bot.message_handler(func=lambda m: True)
